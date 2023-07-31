eba9f19e6449c01ad6376d2d4ca95176bf5c3565
unknown <eske17.ea@gmail.com>
2023-07-31 16:13:47 +0300

    mend

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import { View, FlatList } from "react-native";
 import users from "../../assets/data/users.json";
 import UserListItem from "@/components/UserListItem";
 import { useNavigation } from "expo-router";
